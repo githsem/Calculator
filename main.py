c700084edc3259c6bfe719d2d38de08fd174e6d2
Fahri Cigdem <fahricigdem34@gmail.com>
2020-11-28 23:22:56 +0100

    hoch weg

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from cikarma import cik_f
 from carpma import car_f
 from bolme import bol_f
 from modul import mod_f
-from hoch import hoch_f
 
 
 num1 = int(input("1. Nummer : "))
@@ -14,4 +13,3 @@ print(cik_f(num1, num2))
 print(car_f(num1, num2))
 print(bol_f(num1, num2))
 print(mod_f(num1, num2))
-print(hoch_f(num1, num2))
